395d9f128d6955ef4593ec8279560d5bb404f3cd
hevedrios <exxed.ridel@gmail.com>
2022-08-24 20:34:09 -0500

    Check for error responses and handling 'em

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=;hjksdf;kljsdfgl;kdsjfgljksdfglkjhsdfg")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        throw Error("I'm an error!")
-        // document.body.style.backgroundImage = `url(${data.urls.regular})`
-		// document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        console.log("Something went wrong! 😭")
-        // This is where I can handle the error
-        // Choose to use a default background image
-    })
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=;hjksdf;kljsdfgl;kdsjfgljksdfglkjhsdfg"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    throw Error("I'm an error!");
+    // document.body.style.backgroundImage = `url(${data.urls.regular})`
+    // document.getElementById("author").textContent = `By: ${data.user.name}`
+  })
+  .catch((err) => {
+    // Use a default background image/author
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`;
+    document.getElementById("author").textContent = `By: Dodi Achmad`;
+  });
 
+/**
+ * Challenge: Pull down the cryptocurrency data for dogecoin from the
+ * CoinGecko API and log it to the console
+ *
+ * Also add a .catch() method to console any errors that might occur to the console
+ */
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoinsdfkhsdlfkjhsldkjfhsdf")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    console.log(res.status);
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.error(err));
